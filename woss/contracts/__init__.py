c38e5324e2b8cb45ffc1cc7efd2bdf7137b34b64
Anderson Carlos Woss <anderson.woss@madeiramadeira.com.br>
2021-03-24 18:14:09 -0300

    feat(main): criada a classe Contract

--- woss/contracts/__init__.py
+++ woss/contracts/__init__.py
@@ -1,1 +1,42 @@
-__version__ = '0.0.1'
+"""
+
+"""
+
+import inspect
+
+
+class Contract(type):
+    """
+
+    """
+
+    def __subclasscheck__(self, subclass) -> bool:
+        return all(
+            self.has(subclass, *function) 
+                for function in inspect.getmembers(self, predicate=inspect.isfunction)
+        )
+
+    def __instancecheck__(self, instance) -> bool:
+        return issubclass(instance.__class__, self)
+
+    def has(self, subclass, name, function) -> bool:
+        if not hasattr(subclass, name):
+            return False
+        return self.validate(function, getattr(subclass, name))    
+
+    @property
+    def validators(self):
+        yield from (
+            value for name, value 
+                in inspect.getmembers(inspect, predicate=inspect.isfunction)
+                if name.startswith('is')
+        )
+
+    def validate(self, expected, received) -> bool:
+        if not all(
+            validator(expected) == validator(received) 
+                for validator in self.validators
+        ):
+            return False
+
+        return inspect.signature(expected) == inspect.signature(received)
